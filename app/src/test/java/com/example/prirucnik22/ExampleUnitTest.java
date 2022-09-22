51cddaf5de9f1e6cb078d08d0750ed93804fe94d
1997mirza <1997mirza@gmail.com>
2022-09-22 21:15:35 +0200

    Prirucnik za posmatranje izbora-kod

---
+++ app/src/test/java/com/example/prirucnik22/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.creativocentar.prirucnik22;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * creativocentar local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class creativocentarUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
